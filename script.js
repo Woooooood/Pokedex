53742c16f415950cad94741b0afa8e4bdc6402cc
Woooooood <wassim.boulatika@supinternet.fr>
2018-01-28 15:21:15 +0100

    when user type a pokemon's name or pokemon's id he can see the gif of the pokemon in the pokedex

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 window.onload = function () {
     document.querySelector('#myform').onsubmit = function () {
-        var searchPkm = document.querySelector('input[name="pkdname"]').value;
+        var searchPkm = document.querySelector('input[name="pkdname"]').value.toLowerCase();
         var pokemon = document.querySelector('.screen');
+        var imagepkm = document.querySelector('#img');
 
-            $.ajax({
-                url: 'pokemons.json',
-                type: 'get',
-                dataType: 'json',
-                success: function (data) {
-                    for (var i in data) {
-                       searchPkm += data[i].name;
-                        console.log("test");
+        $.ajax({
+            url: 'pokemons.json',
+            type: 'get',
+            dataType: 'json',
+            success: function (data) {
+                for (var i in data) {
+                    if (searchPkm === data[i].name.toLowerCase() || i === searchPkm) {
+                        imagepkm.setAttribute('src', 'http://www.pokestadium.com/sprites/xy/' + data[i].name.toLowerCase()+ '.gif');
 
                     }
                 }
-            });
+            }
+        });
+        return false;
     }
 }
